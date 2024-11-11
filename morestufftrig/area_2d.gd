960a87c513e8ad7d821b6c1f04d2c5acbd329b05
Ava-TU <C24384981@myTUDublin.ie>
2024-11-11 14:21:01 +0000

    maths using code and using Debug

---
+++ morestufftrig/area_2d.gd
@@ -0,0 +1,55 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	var f:float
+	var g:float
+	var h:float
+	
+	f = 10#assignment
+	g = 20
+	h = 3
+	
+	f += 2# add 2 to f
+	f = f + 2
+	g = f - 5
+	
+	h *= 2#multiple h by 2
+	g = h / 3
+	
+	var i:int
+	var j:int
+	var k:int
+	
+	i = 10
+	j = 4
+	k = i / j
+	
+	print(i)
+	print(j)
+	print(k)
+	
+	print(f)
+	print(g)
+	print(h)
+	
+	
+	
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	DebugDraw2D.set_text("pos", position)
+	DebugDraw2D.set_text("glob_pos", global_position)
+	DebugDraw2D.set_text("rotation", rotation)
+	DebugDraw2D.set_text("glo_rotation", global_rotation)
+	
+	
+	
+	
+	pass
