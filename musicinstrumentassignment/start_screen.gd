94bf3cb40422229baa77f21fed33b517e4867b5e
Ava-TU <C24384981@myTUDublin.ie>
2024-11-18 14:17:14 +0000

    StartScreen Proto

---
+++ musicinstrumentassignment/start_screen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var play: Button = $Play
+@onready var quit: Button = $Quit
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_play_pressed() -> void:
+	
+	get_tree().change_scene_to_file("res://Main.tscn")
+	
+	pass # Replace with function body.
