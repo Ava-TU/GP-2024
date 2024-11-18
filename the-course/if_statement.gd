cd88644d3426d4ef266851f42620350a62d1941e
Ava-TU <C24384981@myTUDublin.ie>
2024-11-18 15:19:00 +0000

    Shows if my mouse is on the left or right side of screen

--- the-course/if_statement.gd
+++ the-course/if_statement.gd
@@ -1,9 +1,20 @@
 extends Node2D
-
+var i = 0
 func _draw() -> void:
+	i = 1
 	var r = get_viewport_rect()
-	r.size.x = r.size.x / 2
-	draw_rect(Rect2(r), Color.RED, true, 10, true)
+	var h = r.size.x / 2
+	
+	
+	var mouse = get_viewport().get_mouse_position()
+	print(mouse)
+	if mouse.x < h:
+		draw_rect(Rect2(0, 0, h, r.size.y), Color.RED, true)
+		draw_rect(Rect2(h, 0, h, r.size.y), Color.BLUE, true)
+	else:
+		draw_rect(Rect2(0, 0, h, r.size.y), Color.BLUE, true)
+		draw_rect(Rect2(h, 0, h, r.size.y), Color.RED, true)
+		
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +23,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	queue_redraw()
 	pass
