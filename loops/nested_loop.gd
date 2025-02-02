2c8ba4792e26ac53f85d682b2e021083b51e06f7
Ava-TU <C24384981@myTUDublin.ie>
2025-02-02 23:02:12 +0000

    nest kinda working

--- loops/nested_loop.gd
+++ loops/nested_loop.gd
@@ -1,19 +1,8 @@
-extends Node2D
+extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func _draw() -> void:
+func _draw () -> void:
 	
 	for x in range(3):
 		for y in range(3):
 			print("(%d, %d)" % [x, y])
-	
-	pass
+		print()
