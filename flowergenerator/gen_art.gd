608d07a236b048cf202f1f273ca33bc61c3c11ec
Ava-TU <C24384981@myTUDublin.ie>
2025-02-06 23:38:45 +0000

    trying to get colours

--- flowergenerator/gen_art.gd
+++ flowergenerator/gen_art.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 var radius:float
 var count:int
+var color_hue_start
+var alpha_inc:float=0.0
+var hue_inc:float=0.0
+var thickness:float = 1
 
 func _draw() -> void:
 	
@@ -15,12 +19,16 @@ func _draw() -> void:
 	var px = 0
 	var py = 0
 	var r = 0
+	var color_hue = color_hue_start
+	var alpha = $"../ColorPickerButton".color.a
 	for i in range(l):
 		var theta = theta_inc * i
 		var x = sin(theta) * r
 		var y = cos(theta) * r
+		var c:Color = Color.from_hsv(color_hue, 1, 1, alpha)
 		draw_line(Vector2(px, py), Vector2(x, y), Color.REBECCA_PURPLE, 10)
-		#draw_circle(Vector2(x, y), 10, Color.CORNSILK, false)
+		draw_circle(Vector2(px, py), 1, c, false, thickness)
+		color_hue += hue_inc * 0.01
 		px = x
 		py = y
 		r = r + radius
@@ -40,4 +48,9 @@ func _on_radius_value_changed(value: float) -> void:
 
 func _on_num_lines_value_changed(value: float) -> void:
 	count = value
+	pass # Replace with function body.
+
+
+func _on_color_picker_button_color_changed(color: Color) -> void:
+	color_hue_start = color.h
 	pass # Replace with function body.
