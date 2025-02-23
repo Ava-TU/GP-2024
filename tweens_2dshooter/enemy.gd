944d373823fcf2438bc389029fbe7151dea2647e
Ava-TU <C24384981@myTUDublin.ie>
2025-02-23 23:56:54 +0000

    enemy one drawn

--- tweens_2dshooter/enemy.gd
+++ tweens_2dshooter/enemy.gd
@@ -11,7 +11,13 @@ func _ready():
 func _draw():
 	# Face
 	draw_circle(Vector2(0, 0), 40, Color.PAPAYA_WHIP, true, 1, true)
+	# Eyebrows
+	draw_line(Vector2(-24, -25), Vector2(-5, -10), Color.SADDLE_BROWN, 2, true)
+	draw_line(Vector2(24, -25), Vector2(5, -10), Color.SADDLE_BROWN, 2, true)
 	# Eyes
 	draw_circle(Vector2(-20, 0), 10, Color.FIREBRICK, false, 3.6, true)
 	draw_circle(Vector2(20, 0), 10, Color.FIREBRICK, false, 3.6, true)
+	# Mouth
+	draw_line(Vector2(-20,28), Vector2(0, 15), Color.MEDIUM_PURPLE, 2, true)
+	draw_line(Vector2(0, 15), Vector2(20, 28), Color.MEDIUM_PURPLE, 2, true)
 	
