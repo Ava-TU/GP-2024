d9529b7598f6026135a9cab281c82b18b35b5db5
Ava-TU <C24384981@myTUDublin.ie>
2025-02-24 00:04:15 +0000

    it moves using tweens

--- tweens_2dshooter/enemy.gd
+++ tweens_2dshooter/enemy.gd
@@ -2,12 +2,6 @@ extends Node2D
 
 var tween: Tween
 
-func _ready():
-	tween = create_tween()
-	tween.set_loops(0)
-
-	
-
 func _draw():
 	# Face
 	draw_circle(Vector2(0, 0), 40, Color.PAPAYA_WHIP, true, 1, true)
@@ -20,4 +14,14 @@ func _draw():
 	# Mouth
 	draw_line(Vector2(-20,28), Vector2(0, 15), Color.MEDIUM_PURPLE, 2, true)
 	draw_line(Vector2(0, 15), Vector2(20, 28), Color.MEDIUM_PURPLE, 2, true)
+	
+func _ready() -> void:
+	tween = create_tween()
+	tween.set_loops(0) # Infinte Loops
+	
+	tween.tween_property(self, "position", position + Vector2(200, 0), 1.0)
+	tween.tween_property(self, "position", position + Vector2(200, 200), 1.0)
+	tween.tween_property(self, "position", position + Vector2(10, -20), 1.0)
+	tween.tween_property(self, "position", position + Vector2(-30, -150), 1.0)
+	tween.tween_property(self, "position", position + Vector2(-60, 200), 1.0)
 	
