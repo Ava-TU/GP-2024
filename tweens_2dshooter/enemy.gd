3c8f5091c6e238f2a04cd03aa06c32b1fe6f08fa
Ava-TU <C24384981@myTUDublin.ie>
2025-02-24 21:37:39 +0000

    easing and trans in tween animation

--- tweens_2dshooter/enemy.gd
+++ tweens_2dshooter/enemy.gd
@@ -18,6 +18,8 @@ func _draw():
 func _ready() -> void:
 	tween = create_tween()
 	tween.set_loops(0) # Infinte Loops
+	tween.set_trans(Tween.TRANS_BACK)
+	tween.set_ease(Tween.EASE_OUT)
 	
 	tween.tween_property(self, "position", position + Vector2(200, 0), 1.0) # Moves postion
 	tween.tween_property(self, "rotation", rotation + 1.5, 1.0) # Rotate 90 Degrees
