864255d09fe0776cc0f2a967711913b3b767cc0d
Ava-TU <C24384981@myTUDublin.ie>
2025-02-24 22:29:59 +0000

    trying to make enemy move in a circle

--- tweens_2dshooter/enemy_2.gd
+++ tweens_2dshooter/enemy_2.gd
@@ -2,6 +2,20 @@ extends Node2D
 
 var tween: Tween
 
+var radius:float = 120
+var angle:float = 0.0
+var speed:float = 1.5
+
+@export var _X_OFFSET = 150
+@export var _Y_OFFSET = 150
+
+func _process(delta):
+	circular_motion()
+	
+func circular_motion():
+	angle += speed * get_process_delta_time()
+	
+
 func _draw():
 	# Body
 	draw_rect(Rect2(0, 40, 45, 60), Color.SKY_BLUE, true, 4, true)
@@ -21,6 +35,9 @@ func _ready() -> void:
 	var tween = create_tween()
 	tween.set_loops(0) # Infinite Loops
 	
-	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 1)
-	tween.tween_property(self, "scale", Vector2(1, 1), 1)
+	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.6)
+	tween.tween_property(self, "scale", Vector2(1, 1), 0.6)
+	tween.set_parallel(true)
+	tween.tween_property(self, "position", position + Vector2(100, 20), 2)
+	
 	
